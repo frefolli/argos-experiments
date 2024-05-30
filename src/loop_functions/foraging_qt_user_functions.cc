158ee4dc855e71e6ef19e95f07959f97e36145ad
Francesco Refolli <refo@LAPTOP-FJTLCSPP.>
2024-05-30 13:16:24 +0200

    ok

---
+++ src/loop_functions/foraging_qt_user_functions.cc
@@ -0,0 +1,33 @@
+#include <loop_functions/foraging_qt_user_functions.hh>
+#include <controllers/footbot_foraging.hh>
+#include <argos3/core/simulator/entity/controllable_entity.h>
+
+using namespace argos;
+
+/****************************************/
+/****************************************/
+
+CForagingQTUserFunctions::CForagingQTUserFunctions() {
+   RegisterUserFunction<CForagingQTUserFunctions,CFootBotEntity>(&CForagingQTUserFunctions::Draw);
+}
+
+/****************************************/
+/****************************************/
+
+void CForagingQTUserFunctions::Draw(CFootBotEntity& c_entity) {
+   CFootBotForaging& cController = dynamic_cast<CFootBotForaging&>(c_entity.GetControllableEntity().GetController());
+   CFootBotForaging::SFoodData& sFoodData = cController.GetFoodData();
+   if(sFoodData.HasFoodItem) {
+      DrawCylinder(
+         CVector3(0.0f, 0.0f, 0.3f), 
+         CQuaternion(),
+         0.1f,
+         0.05f,
+         CColor::BLACK);
+   }
+}
+
+/****************************************/
+/****************************************/
+
+REGISTER_QTOPENGL_USER_FUNCTIONS(CForagingQTUserFunctions, "foraging_qt_user_functions")
