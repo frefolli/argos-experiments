b25bccdd2292c3ee996556a5548dc98e34e753c9
Francesco <f.refolli@campus.unimib.it>
2024-06-01 00:15:58 +0200

    eyebots take off quasi-sequentially

--- include/controllers/eyebot_controller.hh
+++ include/controllers/eyebot_controller.hh
@@ -1,12 +1,19 @@
 #ifndef PREZ_FOOTBOT_HH
 #define PREZ_FOOTBOT_HH
+/** @file */
 #include <argos3/core/control_interface/ci_controller.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_quadrotor_position_actuator.h>
-#include <argos3/plugins/robots/generic/control_interface/ci_quadrotor_speed_actuator.h>
+// #include <argos3/plugins/robots/generic/control_interface/ci_quadrotor_speed_actuator.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_actuator.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_sensor.h>
+// #include <argos3/plugins/robots/eye-bot/control_interface/ci_eyebot_light_sensor.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_positioning_sensor.h>
 #include <cmath>
+#include <random>
+#include <argos3/core/utility/math/rng.h>
 
 namespace prez {
+  /** EyeBot Controller */
   class EyeBotController : public argos::CCI_Controller {
     public:
      EyeBotController();
@@ -14,8 +21,7 @@ namespace prez {
 
      /*
       * This function initializes the controller.
-      * The 't_node' variable points to the <parameters> section in the XML
-      * file in the <controllers><footbot_foraging_controller> section.
+      * The 't_node' variable points to the <parameters> section in the XML file in the <controllers><footbot_foraging_controller> section.
       */
      virtual void Init(argos::TConfigurationNode& t_node);
 
@@ -26,24 +32,45 @@ namespace prez {
      virtual void ControlStep();
 
      /*
-      * This function resets the controller to its state right after the
-      * Init().
+      * This function resets the controller to its state right after the Init().
       * It is called when you press the reset button in the GUI.
       */
      virtual void Reset();
 
      /*
       * Called to cleanup what done by Init() when the experiment finishes.
-      * In this example controller there is no need for clean anything up,
-      * so the function could have been omitted. It's here just for
-      * completeness.
+      * In this example controller there is no need for clean anything up, so the function could have been omitted.
+      * It's here just for completeness.
       */
      virtual void Destroy() {}
     private:
+      /** Move the eyebot to a target position
+       * For some reason you can have either a position_actuator or a speed_actuator, not both.
+      */
       argos::CCI_QuadRotorPositionActuator* position_actuator;
-      argos::CCI_QuadRotorSpeedActuator* speed_actuator;
+      
+      /** Move the eyebot with decided speed vector */
+      // argos::CCI_QuadRotorSpeedActuator* speed_actuator;
+      
+      /** Signals it's state and range to other eyebots */
       argos::CCI_RangeAndBearingActuator* range_and_bearing_actuator;
-      double_t max_speed;
+      
+      /** Recieves state and range of other robots */
+      argos::CCI_RangeAndBearingSensor* range_and_bearing_sensor;
+      
+      /** Returns the position of the closest light object */
+      // argos::CCI_EyeBotLightSensor* eyebot_light_sensor;
+      
+      /** Read current eyebot position */
+      argos::CCI_PositioningSensor* positioning_sensor;
+      
+      /**  Random Number Generator */
+      argos::CRandom::CRNG* random_number_generator;
+
+      /** State enum */
+      enum State {
+        AT_GROUND = 0, TAKING_OFF, TAKEN_OFF
+      } state;
   };
 }
 #endif
--- src/controllers/eyebot_controller.cc
+++ src/controllers/eyebot_controller.cc
@@ -1,26 +1,77 @@
 #include <argos3/core/utility/math/vector3.h>
 #include <controllers/eyebot_controller.hh>
 #include <argos3/core/utility/configuration/argos_configuration.h>
+#include <iostream>
 
 prez::EyeBotController::EyeBotController() :
   position_actuator(nullptr),
-  speed_actuator(nullptr),
+  // speed_actuator(nullptr),
   range_and_bearing_actuator(nullptr),
-  max_speed(1.0f) {}
+  range_and_bearing_sensor(nullptr),
+  // eyebot_light_sensor(nullptr),
+  positioning_sensor(nullptr),
+  random_number_generator(nullptr)
+  {}
 
 void prez::EyeBotController::Init(argos::TConfigurationNode& t_node) {
-  this->position_actuator = GetActuator<argos::CCI_QuadRotorPositionActuator>("position-actuator");
-  this->speed_actuator = GetActuator<argos::CCI_QuadRotorSpeedActuator>("speed-actuator");
-  this->range_and_bearing_actuator = GetActuator<argos::CCI_RangeAndBearingActuator>("range_and_bearing-actuator");
-  argos::GetNodeAttribute(t_node, "max_speed", max_speed);
+  position_actuator = GetActuator<argos::CCI_QuadRotorPositionActuator>("quadrotor_position");
+  // speed_actuator = GetActuator<argos::CCI_QuadRotorSpeedActuator>("quadrotor_speed");
+  range_and_bearing_actuator = GetActuator<argos::CCI_RangeAndBearingActuator>("range_and_bearing");
+  range_and_bearing_sensor = GetSensor<argos::CCI_RangeAndBearingSensor>("range_and_bearing");
+  // eyebot_light_sensor = GetSensor<argos::CCI_EyeBotLightSensor>("eyebot_light");
+  positioning_sensor = GetSensor<argos::CCI_PositioningSensor>("positioning");
+
+  random_number_generator = argos::CRandom::CreateRNG("argos");
+  Reset();
 }
 
 void prez::EyeBotController::ControlStep() {
-  argos::CVector3 pos = {0.0f, 0.0f, 3.0f};
-  this->position_actuator->SetAbsolutePosition(pos);
+  switch (state) {
+    case State::AT_GROUND: {
+      uint32_t count_taking_off = 0;
+      uint32_t count_taken_off = 0;
+      const argos::CCI_RangeAndBearingSensor::TReadings neighbours = range_and_bearing_sensor->GetReadings();
+      for (argos::CCI_RangeAndBearingSensor::SPacket neighbour : neighbours) {
+        if (neighbour.Data[0] == State::TAKING_OFF) {
+          count_taking_off++;
+        } else if (neighbour.Data[0] == State::TAKEN_OFF) {
+          count_taken_off++;
+        }
+      }
+
+      if (count_taking_off == 0) {
+        // Probabilistic backoff, P(take_off) = (count_taken_off+1) / (neighbours.size())
+        if (random_number_generator->Bernoulli((double_t)(count_taken_off+1) / (neighbours.size()))) {
+          // Set target position at above 3.0f meters
+          argos::CVector3 current_position = positioning_sensor->GetReading().Position;
+          argos::CVector3 target_position = current_position;
+          target_position.SetZ(3.0f);
+          position_actuator->SetAbsolutePosition(target_position);
+          
+          // Set state as TAKING_OFF
+          state = State::TAKING_OFF;
+          range_and_bearing_actuator->SetData(0, state);
+        }
+      }
+    }; break;
+    case State::TAKING_OFF: {
+      // If i left 2.0f meters above ground i can consider myself taken off
+      // I maintain current heading target
+      argos::CVector3 current_position = positioning_sensor->GetReading().Position;
+      if (current_position.GetZ() >= 2.0f) {
+        state = State::TAKEN_OFF;
+        range_and_bearing_actuator->SetData(0, state);
+      }
+    }; break;
+    case State::TAKEN_OFF: {
+      // Idle
+      range_and_bearing_actuator->SetData(0, state);
+    }; break;
+  }
 }
 
 void prez::EyeBotController::Reset() {
+  state = State::AT_GROUND;
 }
 
 using namespace prez;
